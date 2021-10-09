d6ee0ce0adc4daed8c61959111d9c951d7289785
Carlos Rodrigo <CRodrigoQuero@GMail.com>
2021-10-09 12:30:46 +0100

    Project naming convention changed for workflow nodes

---
+++ Workflow.States.Generic.Cat.Img/README.md
@@ -0,0 +1,110 @@
+﻿# Acerca de este servicio
+
+Este servicio categoriza imagenes por aspect ratio. Una vez instalado y en funcionamiento,
+cada vez que se cree o se mueva una imagen a su directorio de trabajo (InBox) desde otro directorio, esta sera categorizada de forma inmediata por Aspect Ratio. El servicio crea una carpeta para cada uno de estos ratios y va colocando en cada una de ellas las imagines que se depositan en el mencioando directorio de trabajo.
+
+En esta version, el directorio de trabajo o InBox es C:\Temp\workerservice. Este directorio puedeser configurado al instalar el servicio en Windows.
+
+## Que tiene de especial este servicio?
+
+Este servicio es le primero de una larga serie de servicios que tengo pensado desarrollar. En su estado actual puede ser empleado como plantilla de proyecto Visual Studio 2019 para crear otros servicios. 
+
+Su estructura y comportamiento interno permite que una serie de servicios como este puedan
+ser concatenados para crear un workflow o proceso mucho mas complejo. No sera necesario aplicar cambios en cada servicio de la cadena, ya que la estructura provista por este servicio se adaptara a todas las situaciones, tanto si el servicio en cuestion esta al principio, en medio o al final de la cadena.
+
+El servicio lleva a cabo una accion muy concreta: categrozacion de imagenes por ratio de aspecto, nada especial. La accion principal se ejecuta dentro de una funcion asincrona (ProcessFileAsync). Pero en versiones futuras, tal accion podra ser sustituida por otra a traves de plugins, lo cual quiere decir que se podra modificar el comportamiento, la mision y la utilidad del servicio de forma dinamica a traves de una api, sin ncesidad de tener que instalar el servicio de nuevo. 
+
+Tal caracterisica simplificara el proceso de desarrollo de cualquier workFlow, por extenso o complejo que sea. Permitira tambien a los usuarios poder configurar un workFlow segun sus necesidades concretas mediante una interfaz de usuario basada en web. Esto ultimo implica la generacion y compilacion dinamica de codigo, y una vez llegados a ese punto, puedo intuir que se podran hacer cosas verdaderamente interesantes. 
+
+
+## Publicar el servicio
+
+1. En visual studio 2019, seleccione la opccion "Publish" del menu "Build".
+2. Selccione la opccion pulicar en una carpeta.
+3. Seleccione la ubicacion de la carpeta. 
+4. Pulse el boton "Publish"
+
+Visual studio generara los binarios del servicio necesarios para su instalacion en windows.
+Los comandos de isntalacion deben apuntar al directorio en el que residen los archivos 
+generados durante este proceso de publicacion.
+
+Es recomendable dedicar una carpeta especial para paquetes de sofrtware y dentro de esta, crear una carpeta para los servicios.
+
+La notacion empleada para asignar nombres a los serrvicios es muy importante.
+
+Por ejemplo se puede emplear la siguiente estructura:
+
+- [Nombre del compania]
+	- Aplicaciones
+		- Servicios
+			- [nombre del servicio]
+
+
+## Instalar el servicio
+```ssh
+sc.exe create C4Monitor binpath= "C:\Temp\workerservice\C4imagingNetCore.Workflow.Srv.exe C:\Temp\workerservice\WorkerInbox C:\Temp\workerservice\WorkerOutbox"
+```
+Como puede verse, el nombre del servicio y los parametros necesarios para que el servicio arranque (los mismos que escribimos en la linea de comandos cuando ejecutamos el servicio como aplicacion de consola) se pasan en la misma cadena.
+
+## Arrancar el servicio
+```ssh
+start-service C4Monitor
+```
+## Parar el servicio
+```ssh
+stop-service C4Monitor
+```
+## Borrar el servicio
+```ssh
+sc.exe delete C4Monitor
+```
+## Configuar el servicio para auto-arranque automatico despues de error
+
+Este servicio debe ser configurado para que arranque automaticamente tras un error de ejecucion. El servicio fallara cada vez que encuentre una nueva categoria de imagen. Esto es absolutamente normal.
+
+Este comportamiento ha sido establecido intencionalmente de forma que cada vez que se encuentreuna nueva categoria el servicio vuelva a arrancar para poder asi establecer los watchers para cada nueva categroia.
+
+El mencionado comportamiento debe ser establecido al isntalar el servicio en windows, mediante el siguiente comando:
+```ssh
+SC.exe failure C4Monitor reset= 86400 actions= restart/1000/restart/1000/restart/1000
+```
+Como se puede ver, el comando establece el comportamiento del servicio ante los fallos. El
+servicio volvera arrancar de nuevo inmediatamente tras porducirse un error de ejecucion.
+
+
+## Asignar una categoria al servicio
+```ssh
+sc.exe config c4Monitor group= workflows
+```
+
+# Considreaciones acerca de los nombres del los servicios
+
+Sea cuidadoso en la asignacion de nombres de los servicios, procure que el nombre del servicio sea consistente 
+con los otros servicios existnetetes usando notacion de dominio:
+
+**_[nombre compania]_.wf.srv._[categoria]_._[funcionalidad/nombre servicio]_**
+
+o bien:
+
+**Workflows.srv._[categoria].[funcionalidad/nombre servicio]_**
+
+Ejemplos:
+
+__miCompania.wf.srv.img.categorization.byAspectRatio__
+
+__workFlow.srv.img.categorization.byAspectRatio__
+
+Y asi, subsiguientes servicios prodrian ser nombrados como:
+
+__workFlow.srv.img.categorization.bySizeGroups__
+
+__workFlow.srv.img.categorization.byCountry__
+
+## Proximos pasos
+
+Me gustaria desarrollar algunas funciones mas antes de pasar a la version 2.
+
+- Enviar imagenes por lotes usando archivos zip para su proceso en paralelo.
+- Desarrollar otros servicios parecidos para catalogar imagenes con tros criterios y asi poder generar un workflow de cetegorizacion de imagenes completo a modo de ejemplo.
+- Carga dinamica de plugins de proceso.
+- API para el control del workflow. 
---
+++ Workflow.States.Generic.Cat.Img/Worker.cs
@@ -0,0 +1,264 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using C4ImagingNetCore.Backend;
+using static C4ImagingNetCore.Backend.AspectRatioAnaliser;
+using System.Collections.Generic;
+
+namespace Workflow.States.Generic.Cat.Img
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private HttpClient client;
+        private readonly CommandLineOptions _commandLineOptions;
+
+        private enum WorkerStartupInputType { standard=1, multicategory}
+        WorkerStartupInputType _workerStartupInputType = WorkerStartupInputType.standard;
+
+        public Worker(ILogger<Worker> logger, CommandLineOptions commandLineOptions)
+        {
+            _logger = logger;
+            _commandLineOptions = commandLineOptions;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            client.Dispose();
+            _logger.LogInformation("The service has been stopped at " + DateTimeOffset.Now);
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Service started");
+
+            if (!Directory.Exists(_commandLineOptions.Path))
+            {
+                _logger.LogError($"Inbox Directory \"{_commandLineOptions.Path}\" does not exist.");
+                return;
+            }
+
+            if (!Directory.Exists(_commandLineOptions.OutputPath))
+            {
+                _logger.LogError($"Outbox Directory \"{_commandLineOptions.OutputPath}\" does not exist.");
+                return;
+            }
+
+            // CRUCIAL: PROCESS PRE-EXISTING/REMAINING FILES FROM PREVIOUS EXECUTIONS
+            string[] fileEntries = Directory.GetFiles(_commandLineOptions.Path);
+            foreach (string fileEntry in fileEntries) // get all the files from the inbox ...
+            {
+                // and process them, one by one
+               await ProcessFileAsync(fileEntry);
+            }
+
+            ///var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            //averiguar si el directorio raiz tiene subsdirectorios
+            DirectoryInfo directory = new DirectoryInfo(_commandLineOptions.Path);
+            DirectoryInfo[] subDirectories = directory.GetDirectories();
+
+            _logger.LogInformation(_commandLineOptions.Path + " has "+ subDirectories.Length + " subdirectories:" );
+ 
+            // we need to setup the type of data entry for this service based on if the inbox folder
+            // has subdirectories (the possible input types can be diverse) or not
+            if (subDirectories.Length > 0) _workerStartupInputType = WorkerStartupInputType.multicategory;
+            _logger.LogInformation("Assumend " + _workerStartupInputType.ToString() + " input");
+
+            //root directory
+            using FileSystemWatcher watcher = new FileSystemWatcher
+            {
+                Path = _commandLineOptions.Path,
+                IncludeSubdirectories = false // important to avoid file IO exceptions
+            };
+
+            //the watcher is for monitor new directories created in the root directory
+            using FileSystemWatcher subFolderWatcher = new FileSystemWatcher
+            {
+                Path = _commandLineOptions.Path,
+                IncludeSubdirectories = false // important to avoid file IO exceptions
+            };
+
+            // setup the watcher to watch for new directories only...
+            subFolderWatcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName;
+            
+            // ... and assign an event handler to it
+            subFolderWatcher.Created += async (object sender, FileSystemEventArgs e) =>
+            {
+                await Task.Delay(1000); // instead using delays, here we can implement some retry logic
+                _logger.LogWarning("NEW SUBCATEGORY FOUND: '" + e.Name +  "' (Subdirectory created) ");
+
+                // WARNING: Now, we need to restart the service (!) otherwise, new incoming files in such new category 
+                // will not get processed, because thre are not event handler for them.
+                // This situation is going to occur very often at the first workflow executions, until
+                // the workflow have been "learned" all the possible categories:
+                if(_workerStartupInputType == WorkerStartupInputType.standard)
+                {
+                    // YOU CAN COMMENT THE FOLLOWING LINE IN DEBUG TIME (to make your life eassier):
+                    Environment.Exit(1); // CRUCIAL: this, will make the service to restart automatically
+                }
+
+            };
+
+            // and fianlly, activate the subfolder watcher
+            subFolderWatcher.EnableRaisingEvents = true;
+
+            watcher.Created += async (object sender, FileSystemEventArgs e) =>
+            {
+                await Task.Delay(1000);
+                await ProcessFileAsync(e.FullPath);
+            };
+
+            // create a directory watchers list
+            List<FileSystemWatcher> watchersList = new List<FileSystemWatcher>();
+            foreach (DirectoryInfo dir in subDirectories)
+            {
+                watchersList.Add(new FileSystemWatcher { Path = _commandLineOptions.Path + @"\"   + dir.Name, IncludeSubdirectories = false });
+            };
+
+            //add file extensions filter and an eventhandler to each subfolder watcher
+            foreach (FileSystemWatcher watcherItem in watchersList)
+            {
+                foreach (var extension in _commandLineOptions.Extensions)
+                {
+                    watcherItem.Filters.Add($"*.{extension}");
+                }
+
+                watcherItem.Created += async (object sender, FileSystemEventArgs e) =>
+                {
+                    await Task.Delay(1000);
+                    await ProcessFileAsync(e.FullPath);
+                };
+
+                _logger.LogInformation($"Listening for images created in \"{watcherItem.Path}\"...");
+                watcherItem.EnableRaisingEvents = true; //activating the fieWatcher for each subdirectoy
+            };
+
+            // setup the kind of input we are going to receive
+            _workerStartupInputType = WorkerStartupInputType.standard;
+
+            foreach (var extension in _commandLineOptions.Extensions)
+            {
+                watcher.Filters.Add($"*.{extension}");
+            }
+
+            watcher.EnableRaisingEvents = true; //activating the fieWatcher for root subdirectory
+            _logger.LogInformation($"Listening for images created in \"{_commandLineOptions.Path}\"...");
+
+
+            var tcs = new TaskCompletionSource<bool>();
+            stoppingToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            await tcs.Task;
+
+        }
+
+        private async Task<int> ProcessAllExistingFilesAsync()
+        {
+            int count = 0;
+            foreach (var extension in _commandLineOptions.Extensions)
+            {
+                foreach (var filePath in Directory.EnumerateFiles(_commandLineOptions.Path, $"*.{extension}", SearchOption.TopDirectoryOnly))
+                {
+                    if (await ProcessFileAsync(filePath))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+
+        private Task<bool> ProcessFileAsync(string filePath)
+        {
+            ImageCategorizationResults imgCategoryzationResults = new ImageCategorizationResults();
+
+            try
+            {
+                _logger.LogInformation("AN IMAGE HAS BEEN RECEIVED:" + filePath);
+
+                // call the Backend dll and ctegorize the received file
+                imgCategoryzationResults = GetImageAspectRatio(filePath);
+
+                // analise the categaroization results and log them
+                foreach (ImageCategorizationResult imgCategoryzationResult in imgCategoryzationResults.List)
+                {
+                    // Image analysis, exception handling and logging
+                    if (imgCategoryzationResult.HasException)
+                    {
+                        _logger.LogWarning("Exception: " + imgCategoryzationResult.ExceptionDescription);
+                    }
+                    else
+                    {
+                        _logger.LogInformation("AN IMAGE HAS BEEN CATEGORIZED BY ASPECT RATIO: " + Path.GetFileName(filePath) + " IS " + imgCategoryzationResult.ImageCategory, imgCategoryzationResult.LogId);
+                    }
+                }
+
+                // proceed to move the images to its correspondent folders based on
+                // its category, by analisysng the ImageCategorizationResults
+                foreach (ImageCategorizationResult imgCategoryzationResult in imgCategoryzationResults.List)
+                {
+                    // Create folder
+                    if (!Directory.Exists(imgCategoryzationResult.ImageCategory))
+                    {
+                        Directory.CreateDirectory(imgCategoryzationResult.ImageCategory).ToString();
+                    }
+
+                    MoveFileToFolder(filePath, imgCategoryzationResult.ImageCategory);
+
+                    // and log it
+                    _logger.LogInformation("AN IMAGE HAS BEEN MOVED: " + filePath + " TO .\\" + imgCategoryzationResult.ImageCategory);
+                }
+
+                // now, the categorized images are ready for another and more accurate
+                // categorization performed by another background worker like this one, which can live in the
+                // same server on in a remote one.
+
+                // NEXT, PLEASE! 
+                return Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError( "FILE EXCEPTION: " + ex.Message + ": FILE NAME: " + Path.GetFileName(filePath), $"Error when processing file {filePath}");
+                return Task.FromResult(false);
+            }
+
+        }
+
+        private void MoveFileToFolder(string filePath, string folderName)
+        {
+            var directory = Path.GetDirectoryName(filePath);
+            var fileName = Path.GetFileName(filePath);
+            var destinationDirectory = Path.Combine(directory, folderName);
+
+            Directory.CreateDirectory(destinationDirectory);
+
+            try
+            {
+                File.Move(filePath, Path.Combine(destinationDirectory, fileName), false);
+            }
+            catch (Exception ex)
+            {
+                File.Delete(filePath);
+                throw ex; // passing the exception to higher level (for logging it)
+            }
+
+         }
+
+    }
+}
