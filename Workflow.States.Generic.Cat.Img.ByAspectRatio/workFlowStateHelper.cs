ba426135581dc00b7f433a1ce0558b5c7863fb1a
Carlos Rodrigo <CRodrigoQuero@GMail.com>
2021-10-13 16:26:14 +0100

    New method added to Wokflow helper class

--- Workflow.States.Generic.Cat.Img.ByAspectRatio/workFlowStateHelper.cs
+++ Workflow.States.Generic.Cat.Img.ByAspectRatio/workFlowStateHelper.cs
@@ -27,13 +27,11 @@ namespace Workflow.States.Generic.Cat.Img.ByAspectRatio
 
             return false;
         }
-
         public int GetWorkFlowStateOrderNumberFromDirectoryPath(string diretoryPath)
         {
             diretoryPath = diretoryPath.Replace(@_workflowStateInboxPath, "");
             return diretoryPath.Split(@"\").Length - 1;
         }
-
         public bool WorkFlowStateHasSubdirectories(int workFlowStateNumber)
         {
             if (GetworkFlowStateOutputDirectories(workFlowStateNumber).Length == 0)
@@ -43,7 +41,6 @@ namespace Workflow.States.Generic.Cat.Img.ByAspectRatio
 
             return true;
         }
-
         public string[] GetworkFlowStateInputDirectories(int workFlowStateNumber, bool getFullPath = false)
         {
             string[] directories = GetworkFlowStateOutputDirectories(workFlowStateNumber - 1, getFullPath);
@@ -55,7 +52,6 @@ namespace Workflow.States.Generic.Cat.Img.ByAspectRatio
 
             return directories;
         }
-
         public string[] GetworkFlowStateOutputDirectories(int workFlowStateNumber, bool GetFullPath = false)
         {
             List<WorkFlowStateDirectory> workFlowStateDirectories = new List<WorkFlowStateDirectory>();
@@ -82,5 +78,22 @@ namespace Workflow.States.Generic.Cat.Img.ByAspectRatio
 
             return workFlowStateDirectories.Where(d => d.Level == workFlowStateNumber).Select(x => x.Path).ToArray();
         }
+        public int GetworkFlowActiveStates()
+        {
+            List<WorkFlowStateDirectory> workFlowStateDirectories = new List<WorkFlowStateDirectory>();
+
+            string[] fileEntries = Directory.GetDirectories(@_workflowStateInboxPath, "*.*", SearchOption.AllDirectories);
+            foreach (string fileEntry in fileEntries) // get all the files from the inbox ...
+            {
+                WorkFlowStateDirectory workFlowStateDirectory = new WorkFlowStateDirectory();
+                workFlowStateDirectory.Path = fileEntry.ToString().Replace(@_workflowStateInboxPath, "");
+                workFlowStateDirectory.Level = workFlowStateDirectory.Path.Split(@"\").Length - 1;
+
+                workFlowStateDirectories.Add(workFlowStateDirectory);
+            }
+
+            return workFlowStateDirectories.OrderByDescending(x => x.Level).Select(x => x.Level).FirstOrDefault();
+        }
+
     }
 }
