46aaa61d1886cb0950c408ecd88a810805ff1500
Carlos Rodrigo <CRodrigoQuero@GMail.com>
2021-10-13 09:17:27 +0100

    Added new files

---
+++ Workflow.States.Generic.Cat.Img.ByAspectRatio/WorkFlowStateDirectory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workflow.States.Generic.Cat.Img.ByAspectRatio
+{
+    public class WorkFlowStateDirectory
+    {
+        public string Path { get; set; }
+
+        public int Level { get; set; }
+
+    }
+}
