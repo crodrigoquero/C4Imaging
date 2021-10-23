d94031340ca5d77bf60a1dc0628f90d14dcde664
Carlos Rodrigo <CRodrigoQuero@GMail.com>
2021-10-23 13:32:57 +0100

    New Workflow node to categorize images by gro location ready to its development

--- Workflow.States.Generic.Cat.Img.ByAspectRatio/Program.cs
+++ Workflow.States.Generic.Cat.Img.ByAspectRatio/Program.cs
@@ -1,8 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Serilog;
 using Serilog.Events;
